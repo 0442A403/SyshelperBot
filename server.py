d6cd07a4aaf1179ede199e06cfdbbca724a2c7ad
Damir Petrov <petrovdamir2235@gmail.com>
2020-07-14 06:44:11 +0300

    Add SQLite support

--- server.py
+++ server.py
@@ -2,42 +2,15 @@ from datetime import datetime
 import telebot
 from telebot import types
 import random
+import sqlite3
 
 bot = telebot.TeleBot('1140733846:AAHW8980Rv0ZF4BweijW16XHQctcpqRLyu0')
 sysadmins = { "O442A4O3", "miska924" }
-problems = {}
 
 class Problem:
-    state=0
-    active_sa=None
-    from_user=None
-    occurence_time=None
-    binding_time=None
-    sysadmin=None
-    report_time=None
-    report=None
-    bot_message_id=None
-    chat_id=None
-    
-    def __init__(self, occurence_time, from_user, bot_message_id, chat_id):
-        self.occurence_time = occurence_time
-        self.from_user = from_user
-        self.bot_message_id = bot_message_id
-        self.chat_id = chat_id
-
-    def bind(self, time, sysadmin):
-        if self.state != 0:
-            return
-        self.state = 1
-        self.sysadmin = sysadmin
-        self.binding_time = time
-
-    def report(self, time, report):
-        if self.state != 1:
-            return
-        self.state = 2
-        self.report_time = time
-        self.report = report
+    def __init__(self, input):
+        self.id, self.state, self.from_user, self.occurence_time, self.sysadmin, self.report_time, self.report, self.bot_message_id, self.chat_id = input
+        
 
 @bot.message_handler(commands=['help'])
 def start_message(message):
@@ -115,7 +88,7 @@ def problem_message(message):
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.add(types.InlineKeyboardButton('Заняться проблемой',callback_data=id))
     msg = bot.send_message(message.chat.id, text, reply_markup=keyboard)
-    add_problem(id, datetime.utcnow(), message.from_user.username, msg.message_id, message.chat.id)
+    add_problem(id, datetime.utcnow(), message.from_user.id, msg.message_id, message.chat.id)
 
 @bot.callback_query_handler(func=lambda call: call.data)
 def bind_problem_query(call):
@@ -125,9 +98,10 @@ def bind_problem_query(call):
         return
     if call.from_user.username in sysadmins:
         if bind_problem(call.data, datetime.utcnow(), call.from_user.id):
+            problem = get_problem(call.data)
             bot.edit_message_text(call.message.text + "\nПроблему решает @" + call.from_user.username + ".", 
-                    chat_id=problems[call.data].chat_id, 
-                    message_id=problems[call.data].bot_message_id)
+                    chat_id=problem.chat_id, 
+                    message_id=problem.bot_message_id)
         else:
             bot.answer_callback_query(callback_query_id=call.id, text="Проблему уже кто-то решает.")
     else:
@@ -155,12 +129,13 @@ def report_message(message):
     if not id:
         bot.send_message(message.chat.id, "Пожалуйста, укажите id проблемы; его можно найти в соответствующем сообщении бота.\nПример: /get_report@SysadminHelpBot 123456789")
         return
-    if id not in problems:
-        bot.send_message(message.chat.id, "Неверный id")
+    problem = get_problem(id)
+    if not problem:
+        print("Неверный id")
         return
-    if problems[id].sysadmin != message.from_user.id:
+    if str(problem.sysadmin) != str(message.from_user.id):
         bot.send_message(message.chat.id, "Извините, вы не можете отправлять отчет по этому кейсу, так как кейс был взят другим сисадмином")
-        print(message.from_user.id, problems[id].sysadmin)
+        print(message.from_user.id, problem.sysadmin)
         return
     report_problem(id, datetime.utcnow(), report)
 
@@ -179,30 +154,71 @@ def get_report_message(message):
     if not id.isdigit():
         bot.send_message(message.chat.id, "Пожалуйста, укажите id проблемы; его можно найти в соответствующем сообщении бота.\nПример: /get_report@SysadminHelpBot 123456789")
         return
-    if id not in problems:
+    problem = get_problem(id)
+    if not problem:
         bot.send_message(message.chat.id, "Неверный id")
         return
-    if message.from_user.id != problems[id].sysadmin:
+    if str(message.from_user.id) != str(problem.sysadmin):
         bot.send_message(message.chat.id, "Извините, вы не можете запросить отчет по этому кейсу, так как кейс был взят другим сисадмином")
+        print(message.from_user.id, problem.sysadmin)
         return
-    if problems[id].state != 2:
+    if problem.state != 2:
         bot.send_message(message.chat.id, "Отчет по кейсу не сдан")
         return
-    bot.send_message(message.chat.id, get_report(id))
+    bot.send_message(message.chat.id, "Пустой отчет" if not problem.report else problem.report)
 
-def add_problem(id, time, username, message_id, chat_id):
-    problems[id]=Problem(time, username, message_id, chat_id)
+def add_problem(id, time, user_id, message_id, chat_id):
+    with sqlite3.connect("sysadmin.db") as db_connection:
+        cursor = db_connection.cursor()
+        cursor.execute('''INSERT INTO problems(id, state, from_user, occurence_time, bot_message_id, chat_id) 
+                          VALUES(?, ?, ?, ?, ?, ?)''', (str(id), 0, str(user_id), str(time), str(message_id), str(chat_id)))
+        for i in db_connection.execute("SELECT * FROM problems").fetchall():
+            print(i)
 
 def bind_problem(id, time, user):
-    if problems[id].state != 0:
-        return False
-    problems[id].bind(time, user)
-    return True
+    with sqlite3.connect("sysadmin.db") as db_connection:
+        cursor = db_connection.cursor()
+        rows = cursor.execute('''SELECT state FROM problems WHERE id = ?''', (id,)).fetchall()
+        if len(rows) == 0:
+            return False
+        if rows[0][0] != 0:
+            return False
+        cursor.execute('''UPDATE problems
+            SET state = 1,
+                occurence_time = ? ,
+                sysadmin = ?
+            WHERE id = ?''', (time, user, id))
+        return True
 
 def report_problem(id, time, report):
-    problems[id].report(time, report)
+    with sqlite3.connect("sysadmin.db") as db_connection:
+        cursor = db_connection.cursor()
+        cursor.execute('''UPDATE problems
+            SET state = 2,
+                report_time = ?,
+                report = ?
+            WHERE id = ?''', (time, report, id))
 
-def get_report(id):
-    return problems[id].report
+def get_problem(id):
+    with sqlite3.connect("sysadmin.db") as db_connection:
+        cursor = db_connection.cursor()
+        rows = cursor.execute("SELECT * FROM problems WHERE id = ?", (id,)).fetchall()
+        assert len(rows) < 2
+        if len(rows) == 0:
+            return None
+        return Problem(rows[0])
 
+
+db_connection = sqlite3.connect("sysadmin.db")
+cursor = db_connection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS problems
+             (id text, 
+             state integer,
+             from_user text, 
+             occurence_time text, 
+             sysadmin text, 
+             report_time text, 
+             report text, 
+             bot_message_id text, 
+             chat_id text)''')
 bot.polling()
